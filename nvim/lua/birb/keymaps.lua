269337b400d518eb133093f6cba81aa0b6ef07b9
Toasterbirb <git@toasterbirb.com>
2025-05-30 20:41:06 +0300

    Fix the newline replacing keybind

--- nvim/lua/birb/keymaps.lua
+++ nvim/lua/birb/keymaps.lua
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<Esc>", vim.cmd.nohl, { desc = "Clear search highlighting" 
 vim.keymap.set("n", "<leader>ww", vim.cmd.VimwikiIndex, { desc = "Open the vimwiki index page" })
 vim.keymap.set("v", "<leader>cs", ":sort<CR>", { desc = "Sort the selection" })
 vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "Apply fix" })
-vim.keymap.set("v", "<leader>cn", ":s/\\n/ /g<CR>", { desc = "Replace newlines with whitespace" })
+vim.keymap.set("v", "<leader>cn", ":s/\\n/ /g | s/  / /g<CR>", { desc = "Replace newlines with whitespace" })
 vim.keymap.set("n", "<leader>cy", 'gg<S-v>G"+y', { desc = "Copy the entire file to system clipboard" })
 
 -- Split and window movement
