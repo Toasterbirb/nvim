4383d701ee2b3135026ad2ee6615e197092ac7c4
Toasterbirb <git@toasterbirb.com>
2026-02-12 23:23:04 +0200

    Useful markdown keybinds

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,8 @@
 vim.o.linebreak = true
 -- vim.o.number = false
 -- vim.o.relativenumber = false
+
+vim.keymap.set("n", "<leader>fs", ":Telescope heading<CR>", { desc = "Show headings (markdown)" })
+
+vim.keymap.set("n", "<leader>cc", '/```/-<CR>V??+<CR>"+y', { desc = "Copy the current code block to clipboard" })
+vim.keymap.set("n", "<leader>cy", "/```/-<CR>V??+<CR>y", { desc = "Yank the current code block" })
